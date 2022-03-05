6826a300a2db0b1757ffc404b17fbc2def564a0b
emblafant <embla98wallen@gmail.com>
2022-03-05 12:30:28 +0100

    got all sections

--- redirectory.js
+++ redirectory.js
@@ -1,6 +1,11 @@
 //Get all sections
 const sections = document.querySelectorAll("section");
 
+const startSection = document.querySelector("#startSection");
+const confirmationSection = document.querySelector("#confirmationSection");
+const gameSection = document.querySelector("#gameSection");
+const endSection = document.querySelector("#endSection");
+
 //Hide all but active section
 const hideSections = (activeSection) => {
   sections.forEach(section => {
