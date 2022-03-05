bd4baf7a99d8119a3edc950adde2bb6e7fa77c9e
emblafant <embla98wallen@gmail.com>
2022-03-05 12:24:49 +0100

    function to show and hise sections

--- redirectory.js
+++ redirectory.js
@@ -1,2 +1,10 @@
 //Get all sections
 const sections = document.querySelectorAll("section");
+
+//Hide all but active section
+const hideSections = (activeSection) => {
+  sections.forEach(section => {
+    section.classList.add("hidden");
+  });
+  activeSection.classList.remove("hidden");
+}
