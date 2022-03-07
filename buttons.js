89cce4b67a84d3469de00775f4d0792da7f520b9
emblafant <embla98wallen@gmail.com>
2022-03-07 12:17:41 +0100

    added change page functions to buttons

--- buttons.js
+++ buttons.js
@@ -1,4 +1,22 @@
+//----- Navigation buttons
+
+//Start button (start section)
 document.querySelector("#adoptForm").addEventListener("submit", (e) => {
   goToConfirmationSection();
   e.preventDefault();
+})
+
+//Cancel button (confirmation section)
+document.querySelector("#cancelBtn").addEventListener("click", () => {
+  goToStartSection();
+})
+
+//Confirm button (confirmation section)
+document.querySelector("#confirmBtn").addEventListener("click", () => {
+  goToGameSection();
+})
+
+//Start over button (end section)
+document.querySelector("#startOverBtn").addEventListener("click", () => {
+  goToStartSection();
 })
