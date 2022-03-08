cebf557bb0b5a4e91ceffe06b12985b1865fc734
emblafant <embla98wallen@gmail.com>
2022-03-08 09:37:16 +0100

    replaced images to working images

--- data.js
+++ data.js
@@ -6,16 +6,16 @@ class gotchi {
 }
 
 const gotchi1 = new gotchi(
-  "https://static.wikia.nocookie.net/tamagotchi/images/1/11/Mametchi_anime_large.png/revision/latest/scale-to-width-down/289?cb=20130920042009", 
+  "http://assets.stickpng.com/images/5a2d8f66b3c4622cbe3597af.png", 
   "http://assets.stickpng.com/images/5a2d8fa9b3c4622cbe3597b3.png"
 );
 
 const gotchi2 = new gotchi(
   "https://e7.pngegg.com/pngimages/812/588/png-clipart-green-monster-eating-burger-kuchipatchi-eating-cookies-at-the-movies-cartoons-thumbnail.png",
-  "http://assets.stickpng.com/images/5a2d8fa9b3c4622cbe3597b3.png"
+  "https://images.saatchiart.com/saatchi/1792643/art/8538417/7602300-HSC00001-7.jpg"
 )
 
 const gotchi3 = new gotchi(
-  "https://static.wikia.nocookie.net/tamagotchi/images/0/0a/Memetchi_anime.png/revision/latest/scale-to-width-down/350?cb=20111010000740",
-  "https://static.wikia.nocookie.net/tamagotchi/images/8/8f/Ura_young_memetchi_sad.png/revision/latest/scale-to-width-down/250?cb=20150929040134"
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0bc60b3d-5d7c-44f8-9371-9b4fd835a6c2/d34imah-1c5af333-e667-496d-8d66-b4cba811e990.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBiYzYwYjNkLTVkN2MtNDRmOC05MzcxLTliNGZkODM1YTZjMlwvZDM0aW1haC0xYzVhZjMzMy1lNjY3LTQ5NmQtOGQ2Ni1iNGNiYTgxMWU5OTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bdx90z6U0i7vFCM4BL4cDf1D03DqGYYZSOjflUdClwk",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6b372577-f8f4-4036-bb6b-0cb1fbbd59a2/dcuwzxu-5c0b31fb-b303-4ff0-bcf6-93a358a1fca1.jpg/v1/fill/w_452,h_478,q_75,strp/sad_memetchi_by_pixelchix33_dcuwzxu-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDc4IiwicGF0aCI6IlwvZlwvNmIzNzI1NzctZjhmNC00MDM2LWJiNmItMGNiMWZiYmQ1OWEyXC9kY3V3enh1LTVjMGIzMWZiLWIzMDMtNGZmMC1iY2Y2LTkzYTM1OGExZmNhMS5qcGciLCJ3aWR0aCI6Ijw9NDUyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.OLxcarsH85Hm7-qnWmMZMLH6KgpGGLP-SRxOm743ZzA"
 )
