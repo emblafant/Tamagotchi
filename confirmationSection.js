16691a084510448801db477872c0bbbbbe11fd45
emblafant <embla98wallen@gmail.com>
2022-03-08 09:46:31 +0100

    -

---
+++ confirmationSection.js
@@ -0,0 +1,8 @@
+//Render right image
+const confirmationSectionImage = () => {
+  const gotchi = JSON.parse(localStorage.getItem("gotchiType"));
+  
+  document.querySelector("#confirmationSectionGotchi");
+}
+
+confirmationSectionImage();
