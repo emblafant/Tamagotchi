ee3becf0249afc8d4542e354c15ff4d9fd73cfbe
emblafant <embla98wallen@gmail.com>
2022-03-11 13:05:54 +0100

    render image game section

--- gameSection.js
+++ gameSection.js
@@ -21,16 +21,47 @@ const getMood = (mood) => {
   return JSON.parse(localStorage.getItem(mood));
 }
 
+//Check if any mood is bad
+const checkGoodMood = () => {
+  const happy = getMood("happy");
+  const notHungry = getMood("notHungry");
+  const notSick = getMood("notSick");
+  const clean = getMood("clean");
+
+  if (happy && notHungry && notSick && clean) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //Render gotchi name
 const gameSectionRenderName = () => {
   const gotchiName = getGotchiName();
   document.querySelector("#gameSectionName").innerText = gotchiName;
 }
 
+//Render gotchi image
+const gameSectionRenderImage = () => {
+  const happy = checkGoodMood();
+  const gotchiImg = document.querySelector("#gameSectionGotchi");
+  const gotchiData = getGotchiType();
+  if (happy) {
+    gotchiImg.src = gotchiData.happy;
+  } else {
+    gotchiImg.src = gotchiData.sad;
+  }
+}
+
 //Check if activesection is game section and render 
 const renderGameSection = () => {
   if (getActiveSection() == "gameSection") {
     gameSectionRenderName();
+
+    //updates everysecond
+    setInterval(function(){
+      gameSectionRenderImage();
+    }, 1000)
   }
 }
 
